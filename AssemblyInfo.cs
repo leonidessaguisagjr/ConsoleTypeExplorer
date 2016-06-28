2820e413d4877a20f4e7b7b2bdff3e7fa38c73a4
Leonides T. Saguisag, Jr <leonidessaguisagjr@yahoo.com>
2016-06-28 15:23:46 -0700

    Add AssemblyInfo.cs file.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,35 @@
+// <copyright file="AssemblyInfo.cs">
+// Copyright (c) 2016 Leonides T. Saguisag, Jr.
+//
+// This file is part of CLITypeExplorer.
+//
+// CLITypeExplorer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLITypeExplorer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CLITypeExplorer.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyright("Copyright (c) 2016 Leonides T. Saguisag, Jr.")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Library for easily inspecting types and namespaces in a C# REPL environment.")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("CLITypeExplorer")]
+[assembly: AssemblyTitle("CLITypeExplorer")]
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: ComVisible(false)]
+
+[assembly: NeutralResourcesLanguageAttribute("en")]
