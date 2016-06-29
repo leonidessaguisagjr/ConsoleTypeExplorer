d585a5131603307826aa9aaf78a5e3897a2263a0
Leonides T. Saguisag, Jr <leonidessaguisagjr@yahoo.com>
2016-06-28 18:09:04 -0700

    Add copyright info to CLITypeExplorer.cs and CLITypeExplorer.proj files.  Remove usage info from CLITypeExplorer.cs since that info is already in README.md.

--- CLITypeExplorer.cs
+++ CLITypeExplorer.cs
@@ -1,11 +1,21 @@
-/*
- * How to use:
- * > #r ".\CLITypeExplorer.dll"
- * > using Name.LeonidesSaguisagJr.CLITypeExplorer;
- * > TypeExplorer.ShowTypeSummary(typeof(TypeExplorer), true);
- * > TypeExplorer.ShowTypeMethods(typeof(TypeExplorer), true);
- * > TypeExplorer.ListNamespaceTypes("System.Globalization", true);
- */
+// <copyright file="CLITypeExplorer.cs">
+// Copyright (c) 2016 Leonides T. Saguisag, Jr.
+//
+// This file is part of CLITypeExplorer.
+//
+// CLITypeExplorer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLITypeExplorer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with CLITypeExplorer.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
 
 namespace Name.LeonidesSaguisagJr.CLITypeExplorer {
     using System;
